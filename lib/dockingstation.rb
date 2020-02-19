8abbd30f90b6264d1199e4947ba1e3cb9dae7863
Hodan <hodanahmed0000@gmail.com>
2020-02-19 14:34:26 +0000

    Completed Chapter 12 - included test & code for when docking station is full

--- lib/dockingstation.rb
+++ lib/dockingstation.rb
@@ -1,14 +1,17 @@
 require_relative 'bike'
 class DockingStation
 attr_reader :bike
-def initialize(amount_of_bikes = 0)
+def initialize(amount_of_bikes = 3)
   @bike = amount_of_bikes
+  @amount_of_bikes = amount_of_bikes
 end
 
 def return_bike
+  if @bike == @amount_of_bikes
+    raise 'Docking Station full'
+  end
   @bike += 1
 end
-
 
 def show_bikes
   @bike
@@ -18,7 +21,7 @@ def release_bike
   if @bike == 0
     raise 'No more Bikes'
   end
- #@bike -= 1
- #bike =  Bike.new
+ @bike -= 1
+ bike =  Bike.new
   end
 end
--- spec/dockingstation_spec.rb
+++ spec/dockingstation_spec.rb
@@ -9,11 +9,15 @@ it {is_expected.to respond_to(:return_bike)}
 describe '#release_bike' do
   it 'can tell user there is no more bikes' do
     dockingstation = DockingStation.new
-
+    dockingstation.bike.times{dockingstation.release_bike}
     expect {dockingstation.release_bike}.to raise_error(RuntimeError, "No more Bikes")
   end
-
 end
 
-
+describe '#return_bike' do
+it 'can tell user docking station is full' do
+  dockingstation = DockingStation.new
+  expect {dockingstation.return_bike}.to raise_error(RuntimeError, "Docking Station full")
+ end
+end
 end
